0795e20792b825afa89bc76da9a87d03038099d9
Manpreetkaur666 <mk27982@gmail.com>
2021-02-18 15:22:11 -0500

    First Commit

---
+++ ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,21 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by Manpreet Kaur on 2021-01-27.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "9404F8F1-CBDE-4DBC-B1F2-71B17FABD5CF"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+    func getCoinPrice(for currency: String) {
+        
+    }
+    
+}
